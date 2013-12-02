7302f35dc8ea90bc521a673199f231f3b98d07f8
Francisco Albarran <pahko.xd@gmail.com>
2013-12-01 23:41:10 -0600

    Week 1 Bottle Framework: URL Handlers

---
+++ week-1-3-Bottle-Framework--URL-Handlers/hello_world.py
@@ -0,0 +1,14 @@
+import bottle
+
+@bottle.route('/')
+def home_page():
+    return "Hello World\n"
+
+
+@bottle.route('/testpage')
+def test_page():
+    return "this is a test page"
+
+
+bottle.debug(True)
+bottle.run(host='localhost', port=8080)
